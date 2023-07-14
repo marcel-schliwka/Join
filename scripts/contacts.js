5245ef335ca684fa9d86926a8b6340a209252f28
Selina Karlin <karlinselina@gmail.com>
2023-07-14 13:51:07 +0200

    continuing styling openOverlayContacts
    new feat: doNotClose(event) is working well

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,13 +1,19 @@
 function openOverlayContact() {
     let overlay = document.getElementById('overlayContact');
+    document.getElementById('overlayContact').classList.remove('d-none');
     overlay.classList.add('pop-up-overlay', 'position-absolute', 'top-0', 'start-0', 'bottom-0', 'end-0');
-    
+
     overlay.innerHTML = /*html*/`
-        <div class="custom-overlay-card px-46 py-66">
+        <div class="custom-overlay-card" onclick="doNotClose(event)">
              
-           <div class="contacts-overlay-left">
+           <div class="contacts-overlay-left rounded-start px-46 py-66">
                 <img src="./img/logo.png">
-                </div>
+                <h1 style="color: white">Add contact</h1>
+                <span style="color: white">Tasks are better with a team!</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="94" height="3" viewBox="0 0 94 3" fill="none">
+                    <path d="M92 1.5L2 1.5" stroke="#29ABE2" stroke-width="3" stroke-linecap="round"/>
+                </svg>
+            </div>
 
            <div class="contacts-overlay-right">
             kontaktformular
@@ -19,4 +25,8 @@ function openOverlayContact() {
 
 function closeOverlay() {
     document.getElementById('overlayContact').classList.add('d-none');
+}
+
+function doNotClose(event) {
+    event.stopPropagation();
 }
