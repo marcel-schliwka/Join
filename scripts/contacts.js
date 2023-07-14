16fa0de2b784b2937f203b23150ddd21fb0257a3
Selina Karlin <karlinselina@gmail.com>
2023-07-14 13:04:06 +0200

    first attempt of styling openoverlay
    now: trying bootstrap

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,3 +1,18 @@
 function openOverlayContact() {
     let overlay = document.getElementById('overlayContact');
+    overlay.classList.add('pop-up-overlay', 'position-absolute', 'top-0', 'start-0', 'bottom-0', 'end-0');
+    
+    overlay.innerHTML = /*html*/`
+        <div class="custom-overlay-card px-46 py-66">
+
+           <div class="contacts-overlay-left">
+                <img src="./img/logo.png">
+           </div>
+
+           <div class="contacts-overlay-right">
+            kontaktformular
+           </div>
+
+        </div>
+    `;
 }
