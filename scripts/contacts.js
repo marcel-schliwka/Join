e5a10de8b509aa76675847bba86da8c3697d66f0
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-07-20 17:22:24 +0200

    show contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,169 +1,180 @@
-let pastelColors = {
-    A: 'rgba(255, 209, 220, 1)', // Light Pink
-    B: 'rgba(255, 230, 186, 1)', // Light Peach
-    C: 'rgba(209, 235, 253, 1)', // Light Blue
-    D: 'rgba(255, 204, 153, 1)', // Light Orange
-    E: 'rgba(227, 242, 253, 1)', // Very Light Blue
-    F: 'rgba(224, 213, 255, 1)', // Very Light Purple
-    G: 'rgba(215, 253, 230, 1)', // Light Mint
-    H: 'rgba(251, 228, 228, 1)', // Light Salmon
-    I: 'rgba(252, 242, 221, 1)', // Light Yellow
-    J: 'rgba(239, 224, 245, 1)', // Light Lavender
-    K: 'rgba(231, 255, 233, 1)', // Light Mint Green
-    L: 'rgba(255, 207, 207, 1)', // Light Pink
-    M: 'rgba(255, 227, 208, 1)', // Light Peach
-    N: 'rgba(207, 227, 255, 1)', // Light Blue
-    O: 'rgba(255, 218, 179, 1)', // Light Orange
-    P: 'rgba(207, 240, 255, 1)', // Light Blue
-    Q: 'rgba(254, 223, 240, 1)', // Light Pink
-    R: 'rgba(252, 222, 222, 1)', // Light Salmon
-    S: 'rgba(222, 222, 255, 1)', // Very Light Purple
-    T: 'rgba(248, 218, 218, 1)', // Light Pink
-    U: 'rgba(255, 235, 153, 1)', // Light Yellow
-    V: 'rgba(236, 240, 255, 1)', // Very Light Blue
-    W: 'rgba(219, 249, 244, 1)', // Light Aqua
-    X: 'rgba(255, 242, 242, 1)', // Light Pink
-    Y: 'rgba(255, 240, 219, 1)', // Light Peach
-    Z: 'rgba(255, 224, 224, 1)', // Light Pink
+// Global Variables
+const dialogContact = document.getElementById("dialogContact");
+const dialogElements = {
+  name: document.getElementById("dialog__name"),
+  email: document.getElementById("dialog__email"),
+  phone: document.getElementById("dialog__phone"),
 };
 
+let pastelColors = {
+  A: "rgba(255, 209, 220, 1)", // Light Pink
+  B: "rgba(255, 230, 186, 1)", // Light Peach
+  C: "rgba(209, 235, 253, 1)", // Light Blue
+  D: "rgba(255, 204, 153, 1)", // Light Orange
+  E: "rgba(227, 242, 253, 1)", // Very Light Blue
+  F: "rgba(224, 213, 255, 1)", // Very Light Purple
+  G: "rgba(215, 253, 230, 1)", // Light Mint
+  H: "rgba(251, 228, 228, 1)", // Light Salmon
+  I: "rgba(252, 242, 221, 1)", // Light Yellow
+  J: "rgba(239, 224, 245, 1)", // Light Lavender
+  K: "rgba(231, 255, 233, 1)", // Light Mint Green
+  L: "rgba(255, 207, 207, 1)", // Light Pink
+  M: "rgba(255, 227, 208, 1)", // Light Peach
+  N: "rgba(207, 227, 255, 1)", // Light Blue
+  O: "rgba(255, 218, 179, 1)", // Light Orange
+  P: "rgba(207, 240, 255, 1)", // Light Blue
+  Q: "rgba(254, 223, 240, 1)", // Light Pink
+  R: "rgba(252, 222, 222, 1)", // Light Salmon
+  S: "rgba(222, 222, 255, 1)", // Very Light Purple
+  T: "rgba(248, 218, 218, 1)", // Light Pink
+  U: "rgba(255, 235, 153, 1)", // Light Yellow
+  V: "rgba(236, 240, 255, 1)", // Very Light Blue
+  W: "rgba(219, 249, 244, 1)", // Light Aqua
+  X: "rgba(255, 242, 242, 1)", // Light Pink
+  Y: "rgba(255, 240, 219, 1)", // Light Peach
+  Z: "rgba(255, 224, 224, 1)", // Light Pink
+};
 
 let contacts = [
-    {
-        'name': 'Maximilian Lis',
-        'email': 'lis@fotoka.com',
-        'number': '+4915797298172',
-    },
-    {
-        'name': 'Andreas Loretto',
-        'email': 'andreas.loretto@gmail.com',
-        'number': '+49150192298172',
-    },
-    {
-        'name': 'Maria Rodriguez',
-        'email': 'maria.rodriguez@example.com',
-        'number': '+34678901234',
-    },
-    {
-        'name': 'Léa Dubois',
-        'email': 'lea.dubois@example.com',
-        'number': '+33123456789',
-    },
-    {
-        'name': 'John Smith',
-        'email': 'john.smith@example.com',
-        'number': '+14155552678',
-    },
-    {
-        'name': 'Satoshi Nakamoto',
-        'email': 'satoshi@bitcoin.com',
-        'number': '+81345678901',
-    },
-    {
-        'name': 'Marta Kowalska',
-        'email': 'marta.kowalska@example.com',
-        'number': '+48123456789',
-    },
-    {
-        'name': 'Luis García',
-        'email': 'luis.garcia@example.com',
-        'number': '+34987654321',
-    },
-    {
-        'name': 'Sophie Martin',
-        'email': 'sophie.martin@example.com',
-        'number': '+33123456789',
-    },
-    {
-        'name': 'Muhammad Khan',
-        'email': 'muhammad.khan@example.com',
-        'number': '+92213567890',
-    },
-    {
-        'name': 'Anna Müller',
-        'email': 'anna.mueller@example.com',
-        'number': '+49123456789',
-    },
-    {
-        'name': 'Javier Fernández',
-        'email': 'javier.fernandez@example.com',
-        'number': '+34987654321',
-    },
-    {
-        'name': 'Emma Wilson',
-        'email': 'emma.wilson@example.com',
-        'number': '+441234567890',
-    },
-    {
-        'name': 'Andrei Popescu',
-        'email': 'andrei.popescu@example.com',
-        'number': '+40123456789',
-    },
-    {
-        'name': 'Julia Petrova',
-        'email': 'julia.petrova@example.com',
-        'number': '+79123456789',
-    },
-    {
-        'name': 'Liam Murphy',
-        'email': 'liam.murphy@example.com',
-        'number': '+353123456789',
-    },
-    {
-        'name': 'Hiroshi Tanaka',
-        'email': 'hiroshi.tanaka@example.com',
-        'number': '+819012345678',
-    },
-    {
-        'name': 'Isabella Rossi',
-        'email': 'isabella.rossi@example.com',
-        'number': '+39123456789',
-    },
-    {
-        'name': 'Mohammed Ahmed',
-        'email': 'mohammed.ahmed@example.com',
-        'number': '+201234567890',
-    }
-]
+  {
+    name: "Maximilian Lis",
+    email: "lis@fotoka.com",
+    number: "+4915797298172",
+  },
+  {
+    name: "Andreas Loretto",
+    email: "andreas.loretto@gmail.com",
+    number: "+49150192298172",
+  },
+  {
+    name: "Maria Rodriguez",
+    email: "maria.rodriguez@example.com",
+    number: "+34678901234",
+  },
+  {
+    name: "Léa Dubois",
+    email: "lea.dubois@example.com",
+    number: "+33123456789",
+  },
+  {
+    name: "John Smith",
+    email: "john.smith@example.com",
+    number: "+14155552678",
+  },
+  {
+    name: "Satoshi Nakamoto",
+    email: "satoshi@bitcoin.com",
+    number: "+81345678901",
+  },
+  {
+    name: "Marta Kowalska",
+    email: "marta.kowalska@example.com",
+    number: "+48123456789",
+  },
+  {
+    name: "Luis García",
+    email: "luis.garcia@example.com",
+    number: "+34987654321",
+  },
+  {
+    name: "Sophie Martin",
+    email: "sophie.martin@example.com",
+    number: "+33123456789",
+  },
+  {
+    name: "Muhammad Khan",
+    email: "muhammad.khan@example.com",
+    number: "+92213567890",
+  },
+  {
+    name: "Anna Müller",
+    email: "anna.mueller@example.com",
+    number: "+49123456789",
+  },
+  {
+    name: "Javier Fernández",
+    email: "javier.fernandez@example.com",
+    number: "+34987654321",
+  },
+  {
+    name: "Emma Wilson",
+    email: "emma.wilson@example.com",
+    number: "+441234567890",
+  },
+  {
+    name: "Andrei Popescu",
+    email: "andrei.popescu@example.com",
+    number: "+40123456789",
+  },
+  {
+    name: "Julia Petrova",
+    email: "julia.petrova@example.com",
+    number: "+79123456789",
+  },
+  {
+    name: "Liam Murphy",
+    email: "liam.murphy@example.com",
+    number: "+353123456789",
+  },
+  {
+    name: "Hiroshi Tanaka",
+    email: "hiroshi.tanaka@example.com",
+    number: "+819012345678",
+  },
+  {
+    name: "Isabella Rossi",
+    email: "isabella.rossi@example.com",
+    number: "+39123456789",
+  },
+  {
+    name: "Mohammed Ahmed",
+    email: "mohammed.ahmed@example.com",
+    number: "+201234567890",
+  },
+];
 
 let contactsSorted;
 
 function init() {
-    renderContactList();
+  renderContactList();
+  startEventListener();
 }
 
 function renderContactList() {
-    document.getElementById('contactsListContainer').innerHTML = '';
+  document.getElementById("contactsListContainer").innerHTML = "";
 
-    sortInitialsGroup();
-    renderInitials();
-    generateCircleColor();
+  sortInitialsGroup();
+  renderInitials();
+  generateCircleColor();
 }
 
 function getFirstLetters() {
-    for (const value of contactsSorted.values()) {
-        for (let i = 0; i < value.length; i++) {
-            const fullName = value[i];
-            const firstNameIni = fullName.name.charAt(0).toUpperCase();
-            const lastNameIni = fullName.name.split(' ').pop().charAt(0).toUpperCase();
-            let firstLetters = firstNameIni + lastNameIni;
-            fullName['firstLetters'] = firstLetters;
-            fullName['colorInitial'] = lastNameIni;
-        }
+  for (const value of contactsSorted.values()) {
+    for (let i = 0; i < value.length; i++) {
+      const fullName = value[i];
+      const firstNameIni = fullName.name.charAt(0).toUpperCase();
+      const lastNameIni = fullName.name
+        .split(" ")
+        .pop()
+        .charAt(0)
+        .toUpperCase();
+      let firstLetters = firstNameIni + lastNameIni;
+      fullName["firstLetters"] = firstLetters;
+      fullName["colorInitial"] = lastNameIni;
     }
+  }
 }
-
 
 /**
  *
  *
  */
 function renderInitials() {
-    getFirstLetters();
-    let container = document.getElementById('contactsListContainer');
+  getFirstLetters();
+  let container = document.getElementById("contactsListContainer");
 
-    for (const [key, value] of contactsSorted.entries()) {
-        container.innerHTML += /*html*/`
+  for (const [key, value] of contactsSorted.entries()) {
+    container.innerHTML += /*html*/ `
             <div id="containerLetter${key}" class="container-letter initial" >
                         ${key}
                 <div>
@@ -171,61 +182,68 @@ function renderInitials() {
                 </div>
             </div>
             `;
-        renderContactsInGroup(key, value);
-    }
+    renderContactsInGroup(key, value);
+  }
 }
 
 /**
- * 
- * @param {*} initials 
- * @param {*} contacts 
+ *
+ * @param {*} initials
+ * @param {*} contacts
  */
 function renderContactsInGroup(initials, contacts) {
-    for (let i = 0; i < initials.length; i++) {
-        const initial = initials[i];
-        let groupContainer = document.getElementById(`containerLetter${initial}`);
+  for (let i = 0; i < initials.length; i++) {
+    const initial = initials[i];
+    let groupContainer = document.getElementById(`containerLetter${initial}`);
 
-        for (let c = 0; c < contacts.length; c++) {
-            const contact = contacts[c];
-            let name = contact.name;
-            let email = contact.email;
-            let firstLetter = contact.firstLetters;
-            let colorSign = contact.colorInitial;
-            groupContainer.innerHTML += /*html*/`
+    for (let c = 0; c < contacts.length; c++) {
+      const contact = contacts[c];
+      let name = contact.name;
+      let email = contact.email;
+      let firstLetter = contact.firstLetters;
+      let colorSign = contact.colorInitial;
+      groupContainer.innerHTML += /*html*/ `
                 <div class="single-contact-card">
                     <div class="circle" id="${colorSign}">
                         ${firstLetter}
                     </div>
 
                     <div class="info">
-                        <h4>${name}</h4>
+                        <h4 class="info__name">${name}</h4>
                         <p>${email}</p>
                     </div>
                 </div>
             `;
-        }
     }
+  }
 }
 
 function generateCircleColor() {
-    Object.keys(pastelColors).forEach(key => {
-        let colorValue = pastelColors[key];
-        let colorSign = key;
-        let elements = document.querySelectorAll('.circle');
-        elements.forEach((element) => {
-            if (element && element.id === colorSign) {
-                element.style.backgroundColor = colorValue;
-            }
-        })
-    })
+  Object.keys(pastelColors).forEach((key) => {
+    let colorValue = pastelColors[key];
+    let colorSign = key;
+    let elements = document.querySelectorAll(".circle");
+    elements.forEach((element) => {
+      if (element && element.id === colorSign) {
+        element.style.backgroundColor = colorValue;
+      }
+    });
+  });
 }
 
 function openOverlayContact() {
-    let overlay = document.getElementById('overlayContact');
-    document.getElementById('overlayContact').classList.remove('d-none');
-    overlay.classList.add('pop-up-overlay', 'position-absolute', 'top-0', 'start-0', 'bottom-0', 'end-0');
+  let overlay = document.getElementById("overlayContact");
+  document.getElementById("overlayContact").classList.remove("d-none");
+  overlay.classList.add(
+    "pop-up-overlay",
+    "position-absolute",
+    "top-0",
+    "start-0",
+    "bottom-0",
+    "end-0"
+  );
 
-    overlay.innerHTML = /*html*/`
+  overlay.innerHTML = /*html*/ `
         <div class="custom-overlay-card" onclick="doNotClose(event)">
              
            <div class="contacts-overlay-left rounded-start px-46 py-66">
@@ -294,71 +312,104 @@ function openOverlayContact() {
 }
 
 function closeOverlay() {
-    document.getElementById('overlayContact').classList.add('d-none');
+  document.getElementById("overlayContact").classList.add("d-none");
 }
 
 function doNotClose(event) {
-    event.stopPropagation();
+  event.stopPropagation();
 }
 
 function addNewContact() {
-    let name = document.getElementById('formName').value;
-    let email = document.getElementById('formEmail').value;
-    let phone = document.getElementById('formPhone').value;
+  let name = document.getElementById("formName").value;
+  let email = document.getElementById("formEmail").value;
+  let phone = document.getElementById("formPhone").value;
 
-    contacts.push({
-        name: name,
-        email: email,
-        number: phone,
-    });
+  contacts.push({
+    name: name,
+    email: email,
+    number: phone,
+  });
 
-    resetForm();
-    closeOverlay();
-    renderContactList();
+  resetForm();
+  closeOverlay();
+  renderContactList();
 }
 
 function resetForm() {
-    document.getElementById('contactForm').reset();
+  document.getElementById("contactForm").reset();
 }
 
 /**
  * This function sorts contacts in groups by initials and pushes it into general variable 'initialsContacts'
  * @param {object} initialsMap - a map of all contacts initials in groups
-                        */
+ */
 function groupInitials() {
-    let initialsMap = new Map();
-    contacts.forEach(obj => {
-        let name = obj.name.trim();
-        let initials = name.charAt(0);
+  let initialsMap = new Map();
+  contacts.forEach((obj) => {
+    let name = obj.name.trim();
+    let initials = name.charAt(0);
 
-        if (!initialsMap.has(initials)) {
-            initialsMap.set(initials, []);
-        }
+    if (!initialsMap.has(initials)) {
+      initialsMap.set(initials, []);
+    }
 
-        initialsMap.get(initials).push(obj);
-    });
+    initialsMap.get(initials).push(obj);
+  });
 
-    return initialsMap;
+  return initialsMap;
 }
 
 function sortInitialsGroup() {
-    let initialsMap = groupInitials();
-    let sortedInitialsMap = new Map([...initialsMap.entries()].sort());
-    let sortedContacts = sortContactsAlphabetically(sortedInitialsMap);
-    contactsSorted = sortedContacts;
+  let initialsMap = groupInitials();
+  let sortedInitialsMap = new Map([...initialsMap.entries()].sort());
+  let sortedContacts = sortContactsAlphabetically(sortedInitialsMap);
+  contactsSorted = sortedContacts;
 }
 
 function sortContactsAlphabetically(sortedInitialsMap) {
-    sortedInitialsMap.forEach((obj, initials) => {
-        obj.sort((a, b) => {
-            if (a.name < b.name) {
-                return -1;
-            }
-            if (a.name > b.name) {
-                return 1;
-            }
-            return 0;
-        });
+  sortedInitialsMap.forEach((obj, initials) => {
+    obj.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
     });
-    return sortedInitialsMap;
+  });
+  return sortedInitialsMap;
+}
+
+// Open Contact
+
+let startEventListener = () => {
+  let cards = document.querySelectorAll(".single-contact-card");
+  cards.forEach((card) => {
+    let clickedCard = card;
+    card.addEventListener("click", () => openContact(clickedCard));
+  });
+};
+
+function openContact(card) {
+  const infoCardName = card.querySelector(".info__name").innerText;
+  let clickedContact = getContact(infoCardName);
+  console.log(clickedContact);
+  changeDialogInfo(clickedContact);
+  dialogContact.show();
+}
+
+function getContact(searchedName) {
+  let filteredContact = contacts.filter(
+    (contact) => contact.name === searchedName
+  );
+  return filteredContact[0];
+}
+
+function changeDialogInfo(contact) {
+  dialogElements.name.innerText = contact.name;
+  dialogElements.email.innerText = contact.email;
+  dialogElements.email.href = `mailto:${contact.email}`;
+  dialogElements.phone.innerText = contact.number;
+  dialogElements.phone.href = `tel:${contact.number}`;
 }
