976b39d9d82ca027524ccc5072d8c53ac99b5de4
KlaraK00 <klara.kneisl.2000@gmail.com>
2023-07-21 22:32:47 +0200

    todays date on deadline

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,9 +1,11 @@
+let date = new Date();
+
 function init() {
     setTime();
+    setDate();
 }
 
 function setTime() {
-    let theCurrentHour = currentHour();
     if (currentHour() < 10) {
         document.getElementById('whichHour').innerHTML = `Good morning,`;
     } else if (currentHour() < 14) {
@@ -18,4 +20,46 @@ function setTime() {
 function currentHour() {
     let date = new Date();
     return date.getHours();
+}
+
+function setDate() {
+    let currentDate = currentMonth() + ' ' + currentDay() + ', ' + currentYear();
+    document.getElementById('whichDate').innerHTML = currentDate;
+}
+
+function currentDay() {
+    return date.getDate();
+}
+
+function currentMonth() {
+    let month = date.getMonth();
+    if (month == 0) {
+        return 'January';
+    } else if (month == 1) {
+        return 'February';
+    } else if (month == 2) {
+        return 'March';
+    } else if (month == 3) {
+        return 'April';
+    } else if (month == 4) {
+        return 'May';
+    } else if (month == 5) {
+        return 'June';
+    } else if (month == 6) {
+        return 'July';
+    } else if (month == 7) {
+        return 'August';
+    } else if (month == 8) {
+        return 'September';
+    } else if (month == 9) {
+        return 'October';
+    } else if (month == 10) {
+        return 'November';
+    } else if (month == 11) {
+        return 'December';
+    };
+}
+
+function currentYear() {
+    return date.getFullYear();
 }
