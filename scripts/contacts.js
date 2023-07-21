6af5992535fe7424877e80b10a050d5e801a3bdd
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-07-21 06:53:00 +0200

    redesigned add contact

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -15,6 +15,12 @@ const editDialogElements = {
   inputEmail: document.getElementById("dialog__editEmailInput"),
   inputPhone: document.getElementById("dialog__editPhoneInput"),
   closeDialog: document.getElementById("dialog__editClose"),
+  saveBtn: document.getElementById("dialog__saveBtn"),
+  deleteBtn: document.getElementById("dialog__deleteBtn"),
+};
+
+const createDialogElements = {
+  createDialog: document.getElementById("dialog__createContact"),
 };
 
 let pastelColors = {
@@ -242,90 +248,6 @@ function generateCircleColor() {
   });
 }
 
-function openOverlayContact() {
-  let overlay = document.getElementById("overlayContact");
-  document.getElementById("overlayContact").classList.remove("d-none");
-  overlay.classList.add(
-    "pop-up-overlay",
-    "position-absolute",
-    "top-0",
-    "start-0",
-    "bottom-0",
-    "end-0"
-  );
-
-  overlay.innerHTML = /*html*/ `
-        <div class="custom-overlay-card" onclick="doNotClose(event)">
-             
-           <div class="contacts-overlay-left rounded-start px-46 py-66">
-                <img src="./img/logo.png">
-                <h1 style="color: white">Add contact</h1>
-                <span style="color: white">Tasks are better with a team!</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="94" height="3" viewBox="0 0 94 3" fill="none">
-                    <path d="M92 1.5L2 1.5" stroke="#29ABE2" stroke-width="3" stroke-linecap="round"/>
-                </svg>
-            </div>
-
-           <div class="contacts-overlay-right">
-
-                <div onclick="closeOverlay()">
-                            <img class="click-to-close" src="./img/icon_clear.png">
-                        </div>
-
-
-                <div class="form-section">
-
-                    <div class="input-container-left">
-                        <img width="120px" height="120px" src="./img/icon_newContact.png" class="ms-20">
-                    </div>
-
-                    <div class="input-container-right">
-
-                        <form id="contactForm">
-
-                        <div class="contact-input">
-                            <input id="formName" type="text" placeholder="Name">
-                            <img class="contact-input-img" id="inputName" src="./img/form_name.png">
-                        </div>
-
-                        <div class="contact-input">
-                            <input id="formEmail" type="email" placeholder="Email">
-                            <img class="contact-input-img" id="inputEmail" src="./img/form_email.png">
-                        </div>
-
-                        <div class="contact-input position-relative">
-                            <input id="formPhone" type="number" placeholder="Phone">
-                            <img class="contact-input-img" id="inputPhone" src="./img/form_phone.png">
-                        </div>
-                        </form>
-
-                        <div class="form-button">
-                            <button class="contact-btn custom-lh-120" onclick="closeOverlay()">
-                                <span>Cancel</span>
-                                <img src="./img/icon_clear.png">
-                            </button>
-
-                            <button class="custom-btn fw-bold custom-lh-120" onclick="addNewContact()">
-                                <span>Create contact</span>
-                                <img src="./img/icon_check.png">
-                            </button>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div>
-    `;
-}
-
-function closeOverlay() {
-  document.getElementById("overlayContact").classList.add("d-none");
-}
-
 function doNotClose(event) {
   event.stopPropagation();
 }
@@ -443,6 +365,7 @@ function deleteContact() {
     "Spliced: ",
     contacts.splice(getContactIndex(dialogElements.name.innerText), 1)
   );
+  renderContactList();
 }
 
 // Edit Contact
@@ -456,4 +379,21 @@ function editContact() {
 
 function closeEditDialog() {
   editDialogElements.editDialog.classList.remove("show-edit-dialog");
+}
+
+function saveEditDialog() {
+  let index = getContactIndex(dialogElements.name.innerText);
+  console.log(contacts[index].name);
+  contacts[index].name = editDialogElements.inputName.value;
+  contacts[index].email = editDialogElements.inputEmail.value;
+  contacts[index].number = editDialogElements.inputPhone.value;
+  renderContactList();
+  changeDialogInfo(contacts[index]);
+  startEventListener();
+  closeEditDialog();
+}
+
+// Create Contact
+function openCreateContact() {
+  createDialogElements.createDialog.classList.add("show-edit-dialog");
 }
