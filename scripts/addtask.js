89ca9970fc2b6d326f9b81f55503b249c8c509b6
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-07-22 13:48:57 +0200

    some changees

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -14,22 +14,20 @@ let categorys = [
   }
 ];
 
+let getContacts = [
+  {
+    name: 'Max Mustermann'
+  },
+  {
+    name: 'Peter Lustig'
+  }
+]
+
 
 function init() {
-  renderCategory();
+  renderContacts();
+  renderCategorys();
 }
-
-function renderCategory() {
-  let category = document.getElementById('category-content');
-
-  for (let i = 0; i < categorys.length; i++) {
-    const category = categorys[i];
-    category.innerHTML += `
-    <option class="option pad-18-21">${category['name']}<div class="" style="color: ${category['color']}"></div> ></option>
-    `;
-  }
-}
-
 
 
 
@@ -84,6 +82,100 @@ function getTaskPrio(button, priority) {
   images[priority].src = `./img/prio_${priority}.png`;
   currentPrio = priority;
 }
+
+
+
+function changeToInput(containerId, buttonId) {
+  let cId = document.getElementById(containerId);
+  let bId = document.getElementById(buttonId);
+  cId.innerHTML = '';
+  bId.innerHTML = '';
+
+  cId.innerHTML += `
+  <div>
+    <input id="generatedInput" placeholder="Enter new contact" class="ol-none b-none">
+  </div>
+  `;
+
+  bId.innerHTML += `
+  <div>
+  <button onclick="clearInput(this)" type="button"><img src="./img/cancel_icon.png"></button>
+  <svg xmlns="http://www.w3.org/2000/svg" width="2" height="31" viewBox="0 0 2 31" fill="none">
+  <path d="M1 0V31" stroke="#D1D1D1"/>
+  </svg>
+  <button type="button"><img src="./img/check_black_icon.png"></button>
+  </div>`;
+}
+
+
+
+
+
+
+
+function renderContacts() {
+  let contacts = document.getElementById('renderContacts');
+  contacts.innerHTML = '';
+
+
+  for (let i = 0; i < getContacts.length; i++) {
+    const contact = getContacts[i];
+    contacts.innerHTML += `
+    <div>
+      <li class="contact-item">${contact['name']} <a onclick="changeCheckbox(${i})"><img class="checkboxImg" id="checkboxImg${i}" src="./img/checkbox.png"></a></li>
+    </div>
+    `;
+  }
+}
+
+function changeCheckbox(i) {
+  let checkboxImg = document.getElementById(`checkboxImg${i}`);
+
+  if (checkboxImg.getAttribute('src') === './img/checkbox.png') {
+    checkboxImg.src = './img/checkbox_checked.png'
+  } else {
+    checkboxImg.src = './img/checkbox.png'
+  }
+}
+
+function getAssignedContacts() {
+  let test = document.querySelectorAll('.checkboxImg');
+  for (let i = 0; i < test.length; i++) {
+    const t = test[i];
+    const source = t['currentSrc'];
+    if (source.includes('/img/checkbox_checked.png')) {
+      let index = i;
+      console.log(getContacts[index]['name']);
+    }
+  }
+}
+
+function renderCategorys() {
+  let category = document.getElementById('renderCategorys');
+  category.innerHTML = '';
+  category.innerHTML = `
+  <div>
+  <li onclick="changeToInput('category-input', 'category-button')">Add new category</li>
+  </div>
+  `;
+
+  for (let i = 0; i < categorys.length; i++) {
+    const cat = categorys[i];
+    category.innerHTML += `<div>
+    <li>${cat['name']} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
+    <circle cx="10" cy="10.5" r="9" fill="${cat['color']}" stroke="white" stroke-width="2" />
+  </svg></li>
+    
+    </div>`;
+  }
+}
+
+
+function clearInput(element) {
+  let input = element.parentNode.parentNode.parentNode.querySelector('input');
+  input.value = '';
+}
+
 
 
 
