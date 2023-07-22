193b5f5db987a6125f8f506179f7b1990950c2cd
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-07-22 11:07:31 +0200

    umgeschrieben

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -1,74 +1,93 @@
-let currentPrio;
+
+
+
+
+
+let categorys = [
+  {
+    name: 'Sales',
+    color: '#FC71FF'
+  },
+  {
+    name: 'Backoffice',
+    color: '#38EAFF'
+  },
+  {
+    name: 'Design',
+    color: '#FFC701'
+  }
+];
+
+
+function init() {
+  renderCategory();
+}
+
+function renderCategory() {
+  let category = document.getElementById('category-content');
+
+  for (let i = 0; i < categorys.length; i++) {
+    const category = categorys[i];
+    category.innerHTML += `
+    <option class="option pad-18-21">${category['name']}<div class="" style="color: ${category['color']}"></div> ></option>
+    `;
+  }
+}
+
+
+
+
+let tasks = [
+  {
+    titel: '',
+    description: '',
+    category: '',
+    categoryColor: '',
+    assigned: [],
+    date: '',
+    prio: '',
+    subtasks: []
+  }
+];
+
+
+
+
+
+
+
+
 
 function addTask() {
 
 }
 
 
+let currentPrio;
 
-function addPrio(button, prio) {
-    let buttons = document.querySelectorAll('.add-task-prio-btn button');
-    let images = {
-        low: document.getElementById('lowImg'),
-        medium: document.getElementById('mediumImg'),
-        urgent: document.getElementById('urgentImg')
-    };
+function getTaskPrio(button, priority) {
+  const buttons = document.querySelectorAll('.prioBtn');
+  let images = {
+    low: document.getElementById('lowImg'),
+    medium: document.getElementById('mediumImg'),
+    urgent: document.getElementById('urgentImg')
+  };
 
-    buttons.forEach(function (btn) {
-        if(btn !== button) {
-            btn.classList.remove('low-active', 'medium-active', 'urgent-active');
-            lowImg.src = './img/prio_low_color.png';
-            mediumImg.src = './img/prio_medium_color.png';
-            urgentImg.src = './img/prio_urgent_color.png';
-        }
-    });
-    button.classList.add(prio + '-active');
-    images[prio].src = `./img/prio_${prio}.png`;
-    currentPrio = prio;
-}
-
-function addInput() {
-    const selector = document.getElementById('visableSelector');
-    const input = document.getElementById('hiddenInput');
-    input.classList.remove('d-none');
-    selector.classList.add('d-none');
+  buttons.forEach(function (btn) {
+    if (btn.classList.contains(priority + '-active')) {
+      btn.classList.remove(priority + '-active');
+      currentPrio;
+    } else if (btn !== button) {
+      btn.classList.remove('low-active', 'medium-active', 'urgent-active');
+      lowImg.src = './img/prio_low_color.png';
+      mediumImg.src = './img/prio_medium_color.png';
+      urgentImg.src = './img/prio_urgent_color.png';
+    }    
+  });
+  button.classList.add(priority + '-active');
+  images[priority].src = `./img/prio_${priority}.png`;
+  currentPrio = priority;
 }
 
 
 
-function clearTaskInput() {
-    document.getElementById('visableSelector').value = '';
-}
-
-function handleCategoryChange(select) {
-    var selectedOption = select.options[select.selectedIndex];
-  
-    if (selectedOption.value === "1") {
-      var input = document.createElement('input');
-      input.type = 'text';
-      input.placeholder = 'Enter new category';
-  
-      var parentElement = select.parentNode; // Speichere das Elternelement
-  
-      input.addEventListener('blur', function() {
-        if (input.value.trim() !== '') {
-          var newOption = document.createElement('option');
-          newOption.value = input.value;
-          newOption.textContent = input.value;
-          newOption.selected = true; // Setze die neue Option als ausgewählt
-          select.insertBefore(newOption, select.lastChild);
-        }
-  
-        parentElement.replaceChild(select, input); // Verwende das gespeicherte Elternelement
-      });
-  
-      input.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-          input.blur();
-        }
-      });
-  
-      parentElement.replaceChild(input, select); // Verwende das gespeicherte Elternelement
-      input.focus();
-    }
-  }
