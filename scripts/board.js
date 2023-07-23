c82b5309dc807cdad6caebe607c0eeb268557dbd
KlaraK00 <klara.kneisl.2000@gmail.com>
2023-07-23 16:02:41 +0200

    drag and drop

--- scripts/board.js
+++ scripts/board.js
@@ -34,9 +34,34 @@ let tasks = [
         date: '03.03.22',
         prio: 'medium',
         subtasks: []
+    },
+    {   
+        id: 3,
+        status: 'awaiting feedback',
+        titel: 'titel4',
+        description: 'description4',
+        category: 'category4',
+        categoryColor: 'categoryColor4',
+        assigned: ['RT', 'OC'],
+        date: '04.03.22',
+        prio: 'medium',
+        subtasks: []
+    },
+    {   
+        id: 4,
+        status: 'done',
+        titel: 'titel5',
+        description: 'description5',
+        category: 'category5',
+        categoryColor: 'categoryColor5',
+        assigned: ['MM', 'MA'],
+        date: '05.03.22',
+        prio: 'low',
+        subtasks: []
     }
 ];
 
+// const searchInput = document.querySelector('[data-search]');
 let currentDraggedElement;
 // let lists = document.getElementsByClassName('list');
 // let boxToDo = document.getElementById('todo');
@@ -64,8 +89,11 @@ function initBoard() {
 function updateHTML(){
     renderAllToDos();
     renderAllInProgress();
+    renderAllAwaitingFeedback();
+    renderAllDone();
 }
 
+// --------------- TODO --------------- \\
 function renderAllToDos() {
     let stillToDo = tasks.filter(t => t['status'] == 'todo');
 
@@ -119,11 +147,7 @@ function htmlTemplateAssignment(element, j) {
         </div>`;
 }
 
-function startDragging(id) {
-    console.log(id);
-    currentDraggedElement = id;
-}
-
+// --------------- IN PROGRESS --------------- \\
 function renderAllInProgress() {
     let stillInProgress = tasks.filter(t => t['status'] == 'in progress');
     document.getElementById('inProgress').innerHTML = '';
@@ -160,6 +184,74 @@ function htmlTemplateInProgress(element, i, priority) {
         </div>`;
 }
 
+// --------------- AWAITING FEEDBACK --------------- \\
+function renderAllAwaitingFeedback() {
+    let stillAwaitingFeedback = tasks.filter(t => t['status'] == 'awaiting feedback');
+    document.getElementById('awaitingFeedback').innerHTML = '';
+    for (let i = 0; i < stillAwaitingFeedback.length; i++) {
+        const element = stillAwaitingFeedback[i];
+        document.getElementById('awaitingFeedback').innerHTML += htmlTemplateAwaitingFeedback(element, i, getPriority(element));
+    
+        let idAssigned = document.getElementById(`assignedAwaitingFeedback${i}`);
+        idAssigned.innerHTML = "";  
+
+        for (let j = 0; j < element['assigned'].length; j++) {
+            idAssigned.innerHTML += htmlTemplateAssignment(element, j);
+        }
+    }
+}
+
+function htmlTemplateAwaitingFeedback(element, i, priority) {
+    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="boxShadow border rounded-5 p-2 my-3 d-flex flex-column align-items-start">
+        <div class="textWhite border rounded-3 px-3 m-2" style="background-color:grey">
+            ${element['category']}
+        </div>
+        <div class="mt-2 mx-2 bold">
+            ${element['titel']}
+        </div>
+        <div class="mx-2 my-1">
+            ${element['description']}
+        </div>
+        <div class="d-flex justify-content-between mx-2 my-1 w-100 pe-4">
+            <div class="d-flex textWhite" id="assignedAwaitingFeedback${i}"></div>
+            <div>${priority}</div>
+        </div>
+    </div>`;
+}
+
+// --------------- DONE --------------- \\
+function renderAllDone() {
+    let isDone = tasks.filter(t => t['status'] == 'done');
+    document.getElementById('done').innerHTML = '';
+    for (let i = 0; i < isDone.length; i++) {
+        const element = isDone[i];
+        document.getElementById('done').innerHTML += htmlTemplateDone(element, i, getPriority(element));
+    
+        let idAssigned = document.getElementById(`assignedDone${i}`);
+        idAssigned.innerHTML = "";  
+        for (let j = 0; j < element['assigned'].length; j++) {
+            idAssigned.innerHTML += htmlTemplateAssignment(element, j);
+        }
+    }
+}
+
+function htmlTemplateDone(element, i, priority) {
+    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="boxShadow border rounded-5 p-2 my-3 d-flex flex-column align-items-start">
+        <div class="textWhite border rounded-3 px-3 m-2" style="background-color:grey">
+            ${element['category']}
+        </div>
+        <div class="mt-2 mx-2 bold">
+            ${element['titel']}
+        </div>
+        <div class="mx-2 my-1">
+            ${element['description']}
+        </div>
+        <div class="d-flex justify-content-between mx-2 my-1 w-100 pe-4">
+            <div class="d-flex textWhite" id="assignedDone${i}"></div>
+            <div>${priority}</div>
+        </div>
+    </div>`;
+}
 
 function startDragging(id) {
     currentDraggedElement = id;
@@ -175,4 +267,10 @@ function allowDrop(ev) {
 
 function moveTo(status) {
     tasks[currentDraggedElement]['status'] = status; // z.B. task mit id 1: Das Feld 'status' ändert sich von 'todo' zu 'in progress'
+    updateHTML();
 }
+
+// searchInput.addEventListener('input', (e) => {
+//     const value = e.target.value;
+//     console.log(value);
+// })
