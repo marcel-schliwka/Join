5e7906c933f5bebbe1132ba7b94ffe0c7abe0449
Selina Karlin <karlinselina@gmail.com>
2023-07-24 13:12:55 +0200

    remove console.log

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -2,7 +2,6 @@ let activeUser;
 
 function authenticate() {
     if (localStorage.getItem('activeUser')) {
-        console.log('HALLO');
         activeUser = localStorage.getItem('activeUser');
     } else {
         window.location.href = 'index.html';
