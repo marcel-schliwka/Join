f8d13427850e6d8273dca2bc95eea63b9840c2f3
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-07-30 18:01:52 +0200

    board responsive

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -201,8 +201,15 @@ document.addEventListener("coloris:change", (event) => {
  * it pushes the color value && the selected category into the category-object
  *
  */
+
+function checkIfCategoryInputExists() {
+  return document.getElementById("generatedInput");
+}
 function addNewCategory() {
-  let input = document.getElementById("generatedInput").value;
+  if (checkIfCategoryInputExists()) {
+    let input = document.getElementById("generatedInput").value;
+  }
+
   let color = document.querySelector(".colorpicker").value;
   let category = document.getElementById("category-input");
   let categoryBtn = document.getElementById("category-button");
@@ -748,5 +755,3 @@ function generateNewSubtaskHTML(i, subt) {
     </div>
     `;
 }
-
-
