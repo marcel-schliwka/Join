ae5668d137304af8a47ef545066aceea8f999373
Selina Karlin <karlinselina@gmail.com>
2023-07-31 17:49:43 +0200

    hhhh

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -260,7 +260,7 @@ function sortContactsAlphabetically(sortedInitialsMap) {
  * @date 7/22/2023 - 3:04:10 PM
  */
 let startEventListener = () => {
-window.addEventListener('resize', () => screenTest(mql));
+  window.addEventListener('resize', () => screenTest(mql));
 
   /* selecting all elements with class of single.contact-card to be saved in variable named cards*/
   let cards = document.querySelectorAll(".single-contact-card");
@@ -445,7 +445,6 @@ function addNewContact() {
     email: createDialogElements.inputEmail.value.trim(),
     number: createDialogElements.inputPhone.value.trim(),
   });
-  console.log(contacts)
   document.getElementById("createContactForm").reset();
   dialogBackground.classList.add("d-none");
   cancelCreateContact();
@@ -475,12 +474,33 @@ function closeDialog() {
 
 
 // Responsive
+
 const dialogOpenElements = document.querySelectorAll('dialog');
 const mql = window.matchMedia('(max-width: 800px)');
 const contactInfo = document.getElementById('contactsMain');
 const respDialogCreateButton = document.getElementById('dialog__createSaveBtn');
-// const respDialogAddContact = document.getElementById('resp_create_contact');
 
+function screenTest(e) {
+  console.log('screentest wird aufgerufen!')
+  if (e.matches) {
+    dialogOpenElements.forEach(dialog => {
+      if (dialog.open) {
+        // dialog.close();
+        dialog.classList.add('d-none');
+      }
+    });
+    // responsiveStartEventListener();
+    contactInfo.classList.add('d-none');
+    respDialogCreateButton.addEventListener('click', responsiveCloseCreateContact);
+    editDialogElements.closeDialog.addEventListener('click', responsiveCloseCreateContact);
+    document.getElementById('dialog__editSaveBtn').addEventListener('click', responsiveCloseCreateContact)
+    document.getElementById('responsiveButton').classList.remove('d-none');
+    responsiveStartEventListener();
+  } else {
+    editDialogElements.closeDialog.removeEventListener('click', responsiveCloseCreateContact);
+    respDialogCreateButton.removeEventListener('click', responsiveCloseCreateContact);
+  }
+}
 
 let responsiveStartEventListener = () => {
   /* selecting all elements with class of single.contact-card to be saved in variable named cards*/
@@ -492,29 +512,9 @@ let responsiveStartEventListener = () => {
     /* calling openContact() when contact card is clicked */
     card.addEventListener("click", () => responsiveOpenContact(clickedCard));
   });
+
+  document.getElementById('dialog__editDeleteBtn').addEventListener('click', () => responsiveDeleteContact())
 };
-
-function screenTest(e) {
-  console.log('screentest wird aufgerufen!')
-  if (e.matches) {
-    responsiveStartEventListener();
-    contactInfo.classList.add('d-none');
-    respDialogCreateButton.addEventListener('click', responsiveCloseCreateContact);
-    editDialogElements.closeDialog.addEventListener('click', responsiveCloseCreateContact);
-    document.getElementById('dialog__editSaveBtn').addEventListener('click', responsiveCloseCreateContact)
-    dialogOpenElements.forEach(dialog => {
-      if (dialog.open) {
-        // dialog.close();
-        dialog.classList.add('d-none');
-      }
-    });
-    document.getElementById('responsiveButton').classList.remove('d-none');
-  } else {
-    editDialogElements.closeDialog.removeEventListener('click', responsiveCloseCreateContact);
-    respDialogCreateButton.removeEventListener('click', responsiveCloseCreateContact);
-  }
-}
-
 
 function responsiveOpenCreateContact() {
   dialogBackground.classList.remove('d-none');
@@ -538,24 +538,13 @@ function responsiveOpenCreateContact() {
 
 }
 
-function responsiveOpenContact(card) {
+function responsiveOpenContact() {
   document.getElementById('contactsMain').classList.remove('d-none');
   document.getElementById('dialogContact').classList.remove('d-none');
-  document.getElementById('openCreateContactButton').classList.add('d-none');
-  document.getElementById('dialog__Buttons').classList.add('d-none');
-  document.getElementById('svgElement').classList.add('d-none');
   document.getElementById('seperatorResponsive').classList.remove('d-none');
+  document.getElementById('arrowResponsive').classList.remove('d-none');
+  document.getElementById('editResponsiveBtn').classList.remove('d-none');
   document.getElementById('subHeaderDialog').classList.add('fs-21');
-
-  document.getElementById('contactsMain').innerHTML += /*html*/`
-    <img id="arrowResponsive" src="./img/arrow_left_responsive.svg" class="position-absolute top-0 end-0 pt-20" onclick="responsiveCloseDialog()">
-  `;
-  document.getElementById('contactsMain').innerHTML += /*html*/`
-  <button id="editResponsiveBtn" onclick="responsiveShowOptions()">
-    <img onmouseover="hover(this);" onmouseout="unhover(this);" src="./img/edit_contact_responsive.svg">
-  </button>
-  <menu id="editOptions" class="d-none"></menu>
-`;
 }
 
 function responsiveCloseCreateContact() {
@@ -568,52 +557,22 @@ function responsiveCloseCreateContact() {
 
 function responsiveCloseDialog() {
   document.getElementById('contactsMain').classList.add('d-none');
-  document.getElementById('arrowResponsive').remove();
-  document.getElementById('editResponsiveBtn').remove();
-  document.getElementById('editOptions').remove();
+  // document.getElementById('arrowResponsive').remove();
+  document.getElementById('editResponsiveBtn').classList.add('d-none')
+  document.getElementById('editOptions').classList.add('d-none');
 }
 
-function hover(element) {
-  element.setAttribute('src', './img/edit_contact_responsive_lightBlue.svg');
-}
+// function hover(element) {
+//   element.setAttribute('src', './img/edit_contact_responsive_lightBlue.svg');
+// }
 
-function unhover(element) {
-  element.setAttribute('src', './img/edit_contact_responsive.svg');
-}
+// function unhover(element) {
+//   element.setAttribute('src', './img/edit_contact_responsive.svg');
+// }
 
 function responsiveShowOptions() {
   document.getElementById('editResponsiveBtn').classList.add('d-none');
-  let options = document.getElementById('editOptions');
-  options.classList.remove('d-none');
-  options.innerHTML = /*html*/`
-  <div class="edit-options">
-
-    <button onclick="responsiveEditContact()">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-    <mask id="mask0_71395_18215" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-      <rect width="24" height="24" fill="#D9D9D9"/>
-    </mask>
-    <g mask="url(#mask0_71395_18215)">
-      <path d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="#2A3647"/>
-    </g>
-    </svg>
-    <span>Edit</span>
-    </button>
-    <button onclick="responsiveDeleteContact()">
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-    <mask id="mask0_71395_18226" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="24">
-      <rect x="0.5" width="24" height="24" fill="#D9D9D9"/>
-    </mask>
-    <g mask="url(#mask0_71395_18226)">
-      <path d="M7.5 21C6.95 21 6.47917 20.8042 6.0875 20.4125C5.69583 20.0208 5.5 19.55 5.5 19V6C5.21667 6 4.97917 5.90417 4.7875 5.7125C4.59583 5.52083 4.5 5.28333 4.5 5C4.5 4.71667 4.59583 4.47917 4.7875 4.2875C4.97917 4.09583 5.21667 4 5.5 4H9.5C9.5 3.71667 9.59583 3.47917 9.7875 3.2875C9.97917 3.09583 10.2167 3 10.5 3H14.5C14.7833 3 15.0208 3.09583 15.2125 3.2875C15.4042 3.47917 15.5 3.71667 15.5 4H19.5C19.7833 4 20.0208 4.09583 20.2125 4.2875C20.4042 4.47917 20.5 4.71667 20.5 5C20.5 5.28333 20.4042 5.52083 20.2125 5.7125C20.0208 5.90417 19.7833 6 19.5 6V19C19.5 19.55 19.3042 20.0208 18.9125 20.4125C18.5208 20.8042 18.05 21 17.5 21H7.5ZM7.5 6V19H17.5V6H7.5ZM9.5 16C9.5 16.2833 9.59583 16.5208 9.7875 16.7125C9.97917 16.9042 10.2167 17 10.5 17C10.7833 17 11.0208 16.9042 11.2125 16.7125C11.4042 16.5208 11.5 16.2833 11.5 16V9C11.5 8.71667 11.4042 8.47917 11.2125 8.2875C11.0208 8.09583 10.7833 8 10.5 8C10.2167 8 9.97917 8.09583 9.7875 8.2875C9.59583 8.47917 9.5 8.71667 9.5 9V16ZM13.5 16C13.5 16.2833 13.5958 16.5208 13.7875 16.7125C13.9792 16.9042 14.2167 17 14.5 17C14.7833 17 15.0208 16.9042 15.2125 16.7125C15.4042 16.5208 15.5 16.2833 15.5 16V9C15.5 8.71667 15.4042 8.47917 15.2125 8.2875C15.0208 8.09583 14.7833 8 14.5 8C14.2167 8 13.9792 8.09583 13.7875 8.2875C13.5958 8.47917 13.5 8.71667 13.5 9V16Z" fill="#2A3647"/>
-    </g>
-    </svg>
-    
-    <span>Delete</span>
-    </button>
-    
-  </div>
-  `;
+  document.getElementById('editOptions').classList.remove('d-none');
 }
 
 function responsiveDeleteContact() {
@@ -625,8 +584,12 @@ function responsiveDeleteContact() {
 }
 
 function responsiveEditContact() {
+  document.getElementById('editResponsiveBtn').classList.remove('d-none');
+
+  let options = document.getElementById('editOptions');
+  options.classList.add('d-none');
   document.getElementById('openEditContactLogo').classList.add('d-none');
-  document.getElementById('dialog__editRightImg').classList.add('d-none');
+  // document.getElementById('dialog__editRightImg').classList.add('d-none');
   editDialogElements.editDialog.classList.remove('d-none');
   dialogBackground.classList.remove("d-none");
   editDialogElements.closeDialog.src = './img/close_white.svg';
