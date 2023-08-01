a3eda26d7079bef9f15d7c57dbc39e279e2a4b67
Selina Karlin <karlinselina@gmail.com>
2023-08-01 19:54:55 +0200

    small cleaning of code in renderContactsInContainer

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -164,323 +164,319 @@ function renderContactsInContainer(initials, contacts) {
     let groupContainer = document.getElementById(`containerLetter${initial}`);
 
     for (let c = 0; c < contacts.length; c++) {
-      const contact = contacts[c];
-      let name = contact.name;
-      let email = contact.email;
       groupContainer.innerHTML += /*html*/ `
         <div class="single-contact-card" id="card${c}">
-            <div class="circle" id="${getColorSign(contact.name)}">
-              ${getInitials(contact.name)}
+            <div class="circle" id="${getColorSign(contacts[c]['name'])}">
+              ${getInitials(contacts[c]['name'])}
             </div>
 
             <div class="info">
-                <h4 class="info__name">${name}</h4>
-                 <p>${email}</p>
+                <h4 class="info__name">${contacts[c]['name']}</h4>
+                 <p>${contacts[c]['email']}</p>
             </div>
          </div>
       `;
     }
   }
 }
-
-/**
- * generating color of circle in ContactList according to initial of surname
- *
- * @function
- */
-function generateCircleColor() {
-  Object.keys(colors).forEach((key) => {
-    let colorValue = colors[key];
-    let colorSign = key;
-    let elements = document.querySelectorAll(".circle");
-    elements.forEach((element) => {
-      if (element && element.id === colorSign) {
-        element.style.backgroundColor = colorValue;
-      }
+  /**
+   * generating color of circle in ContactList according to initial of surname
+   *
+   * @function
+   */
+  function generateCircleColor() {
+    Object.keys(colors).forEach((key) => {
+      let colorValue = colors[key];
+      let colorSign = key;
+      let elements = document.querySelectorAll(".circle");
+      elements.forEach((element) => {
+        if (element && element.id === colorSign) {
+          element.style.backgroundColor = colorValue;
+        }
+      });
     });
-  });
-}
+  }
 
-/**
- * sorting contacts in groups by initials and pushes it into general variable 'initialsContacts'
- * @param {object} initialsMap - a map of all contacts initials in groups
- */
-function groupInitials() {
-  let initialsMap = new Map();
-  contacts.forEach((obj) => {
-    let name = obj.name.trim();
-    let initials = name.charAt(0);
+  /**
+   * sorting contacts in groups by initials and pushes it into general variable 'initialsContacts'
+   * @param {object} initialsMap - a map of all contacts initials in groups
+   */
+  function groupInitials() {
+    let initialsMap = new Map();
+    contacts.forEach((obj) => {
+      let name = obj.name.trim();
+      let initials = name.charAt(0);
 
-    if (!initialsMap.has(initials)) {
-      initialsMap.set(initials, []);
+      if (!initialsMap.has(initials)) {
+        initialsMap.set(initials, []);
+      }
+
+      initialsMap.get(initials).push(obj);
+    });
+
+    return initialsMap;
+  }
+
+  /**
+   * sorting a group of contacts based on their names initial
+   *
+   * @function
+   * @returns updated global object: sortedContacts
+   */
+  function sortInitialsGroup() {
+    let initialsMap = groupInitials();
+    let sortedInitialsMap = new Map([...initialsMap.entries()].sort());
+    let sortedContacts = sortContactsAlphabetically(sortedInitialsMap);
+    contactsSorted = sortedContacts;
+  }
+
+  /**
+   * sorting Contacts withing group (key) alphabetically
+   *
+   * @param {Object} sortedInitialsMap
+   * @returns the values of the Map in alphabetical order
+   */
+  function sortContactsAlphabetically(sortedInitialsMap) {
+    sortedInitialsMap.forEach((obj, initials) => {
+      obj.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+    });
+    return sortedInitialsMap;
+  }
+
+  // Open Contact
+
+  /**
+   * setting up event listeners to single-ontact-cards in contactList as well as in the dialog-window and edit-dialog-window
+   * @date 7/22/2023 - 3:04:10 PM
+   */
+  let startEventListener = () => {
+    let cards = document.querySelectorAll(".single-contact-card");
+    cards.forEach((card) => {
+      let clickedCard = card;
+      card.addEventListener("click", () => openContact(clickedCard));
+    });
+    dialogElements.deleteBtn.addEventListener("click", () => deleteContact());
+    dialogElements.editBtn.addEventListener("click", () => editContact());
+    editDialogElements.closeDialog.addEventListener("click", () =>
+      closeEditDialog()
+    );
+  };
+
+  /**
+   * opening dialog-window to show contact information
+   *
+   * @param {Object} card
+   */
+  function openContact(card) {
+    document.getElementById('contactsMain').classList.remove('contacts-none');
+    document.getElementById('contactsMain').classList.add('contacts-main');
+    document.getElementById('buttonRespBack').classList.remove('d-none');
+    const cardId = card.getAttribute("id");
+    dialogElements["fromCard"] = card;
+    dialogElements["initials"] = card.querySelector(".circle").innerText;
+    dialogElements["circleColor"] = card
+      .querySelector(".circle")
+      .getAttribute("style");
+    dialogElements["profilePic"] = document.querySelector(".dialog__circle");
+    const infoCardName = card.querySelector(".info__name").innerText.trim();
+    let clickedContact = getContact(infoCardName);
+    changeDialogInfo(clickedContact, cardId);
+    dialogContact.show();
+  }
+
+  function getContact(searchedName) {
+    let filteredContact = contacts.filter(
+      (contact) => contact.name === searchedName
+    );
+    return filteredContact[0];
+  }
+
+  /**
+   * getting index of current contact-card
+   *
+   * @param {string} searchedName
+   * @return {number}
+   */
+  function getContactIndex(searchedName) {
+    return contacts.findIndex((contact) => contact.name === searchedName);
+  }
+
+  /**
+   * generating style and rendering HTML for dialog-window with contact information
+   *
+   * @function
+   * @param {Object} contact
+   */
+  function changeDialogInfo(contact) {
+    dialogElements.profilePic.innerHTML = dialogElements.initials;
+    dialogElements.profilePic.style = dialogElements.circleColor;
+    dialogElements.name.innerText = contact.name;
+    dialogElements.email.innerText = contact.email;
+    dialogElements.email.href = `mailto:${contact.email}`;
+    dialogElements.phone.innerText = contact.number;
+    dialogElements.phone.href = `tel:${contact.number}`;
+  }
+
+  /**
+   * deleting contact from contactList and remote storage
+   *
+   * @function
+   */
+  function deleteContact() {
+    dialogElements.fromCard.remove();
+    dialogContact.close();
+    contacts.splice(getContactIndex(dialogElements.name.innerText), 1);
+    deleteInitial(dialogElements.name.innerText);
+  }
+
+  function deleteInitial(name) {
+    let initial = name.trim().charAt(0).toUpperCase();
+    let initialDiv = document.getElementById(`containerLetter${initial}`);
+    if (initialDiv.children.length == 0) {
+      initialDiv.remove();
     }
-
-    initialsMap.get(initials).push(obj);
-  });
-
-  return initialsMap;
-}
-
-/**
- * sorting a group of contacts based on their names initial
- *
- * @function
- * @returns updated global object: sortedContacts
- */
-function sortInitialsGroup() {
-  let initialsMap = groupInitials();
-  let sortedInitialsMap = new Map([...initialsMap.entries()].sort());
-  let sortedContacts = sortContactsAlphabetically(sortedInitialsMap);
-  contactsSorted = sortedContacts;
-}
-
-/**
- * sorting Contacts withing group (key) alphabetically
- *
- * @param {Object} sortedInitialsMap
- * @returns the values of the Map in alphabetical order
- */
-function sortContactsAlphabetically(sortedInitialsMap) {
-  sortedInitialsMap.forEach((obj, initials) => {
-    obj.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    });
-  });
-  return sortedInitialsMap;
-}
-
-// Open Contact
-
-/**
- * setting up event listeners to single-ontact-cards in contactList as well as in the dialog-window and edit-dialog-window
- * @date 7/22/2023 - 3:04:10 PM
- */
-let startEventListener = () => {
-  let cards = document.querySelectorAll(".single-contact-card");
-  cards.forEach((card) => {
-    let clickedCard = card;
-    card.addEventListener("click", () => openContact(clickedCard));
-  });
-  dialogElements.deleteBtn.addEventListener("click", () => deleteContact());
-  dialogElements.editBtn.addEventListener("click", () => editContact());
-  editDialogElements.closeDialog.addEventListener("click", () =>
-    closeEditDialog()
-  );
-};
-
-/**
- * opening dialog-window to show contact information
- *
- * @param {Object} card
- */
-function openContact(card) {
-  document.getElementById('contactsMain').classList.remove('contacts-none');
-  document.getElementById('contactsMain').classList.add('contacts-main');
-  document.getElementById('buttonRespBack').classList.remove('d-none');
-  const cardId = card.getAttribute("id");
-  dialogElements["fromCard"] = card;
-  dialogElements["initials"] = card.querySelector(".circle").innerText;
-  dialogElements["circleColor"] = card
-    .querySelector(".circle")
-    .getAttribute("style");
-  dialogElements["profilePic"] = document.querySelector(".dialog__circle");
-  const infoCardName = card.querySelector(".info__name").innerText.trim();
-  let clickedContact = getContact(infoCardName);
-  changeDialogInfo(clickedContact, cardId);
-  dialogContact.show();
-}
-
-function getContact(searchedName) {
-  let filteredContact = contacts.filter(
-    (contact) => contact.name === searchedName
-  );
-  return filteredContact[0];
-}
-
-/**
- * getting index of current contact-card
- *
- * @param {string} searchedName
- * @return {number}
- */
-function getContactIndex(searchedName) {
-  return contacts.findIndex((contact) => contact.name === searchedName);
-}
-
-/**
- * generating style and rendering HTML for dialog-window with contact information
- *
- * @function
- * @param {Object} contact
- */
-function changeDialogInfo(contact) {
-  dialogElements.profilePic.innerHTML = dialogElements.initials;
-  dialogElements.profilePic.style = dialogElements.circleColor;
-  dialogElements.name.innerText = contact.name;
-  dialogElements.email.innerText = contact.email;
-  dialogElements.email.href = `mailto:${contact.email}`;
-  dialogElements.phone.innerText = contact.number;
-  dialogElements.phone.href = `tel:${contact.number}`;
-}
-
-/**
- * deleting contact from contactList and remote storage
- *
- * @function
- */
-function deleteContact() {
-  dialogElements.fromCard.remove();
-  dialogContact.close();
-  contacts.splice(getContactIndex(dialogElements.name.innerText), 1);
-  deleteInitial(dialogElements.name.innerText);
-}
-
-function deleteInitial(name) {
-  let initial = name.trim().charAt(0).toUpperCase();
-  let initialDiv = document.getElementById(`containerLetter${initial}`);
-  if (initialDiv.children.length == 0) {
-    initialDiv.remove();
+    renderContactList();
   }
-  renderContactList();
-}
 
-// Edit Contact
+  // Edit Contact
 
-/**
- * editing contact within edit-dialog-window
- *
- * @function
- */
-function editContact() {
-  editDialogElements.editDialog.classList.remove('resp-none');
-  editDialogElements.editDialog.classList.add("show-edit-dialog");
-  dialogBackground.classList.remove("d-none");
-  editDialogElements.inputEmail.value = dialogElements.email.innerText;
-  editDialogElements.inputPhone.value = dialogElements.phone.innerText;
-}
+  /**
+   * editing contact within edit-dialog-window
+   *
+   * @function
+   */
+  function editContact() {
+    editDialogElements.editDialog.classList.remove('resp-none');
+    editDialogElements.editDialog.classList.add("show-edit-dialog");
+    dialogBackground.classList.remove("d-none");
+    editDialogElements.inputEmail.value = dialogElements.email.innerText;
+    editDialogElements.inputPhone.value = dialogElements.phone.innerText;
+  }
 
-/**
- * closing edit-dialog-window
- *
- * @function
- */
-function closeEditDialog() {
-  editDialogElements.editDialog.classList.add('resp-none');
-  editDialogElements.editDialog.classList.remove("show-edit-dialog");
-  dialogBackground.classList.add("d-none");
-}
+  /**
+   * closing edit-dialog-window
+   *
+   * @function
+   */
+  function closeEditDialog() {
+    editDialogElements.editDialog.classList.add('resp-none');
+    editDialogElements.editDialog.classList.remove("show-edit-dialog");
+    dialogBackground.classList.add("d-none");
+  }
 
-/**
- * saving edited contact
- *
- * @function
- */
-function saveEditDialog() {
-  let index = getContactIndex(dialogElements.name.innerText);
-  /* changing values in contacts array at the index with the values from input field */
-  contacts[index].name = editDialogElements.inputName.value;
-  contacts[index].email = editDialogElements.inputEmail.value;
-  contacts[index].number = editDialogElements.inputPhone.value;
-  renderContactList();
-  changeDialogInfo(contacts[index]);
-  closeEditDialog();
-}
-
-// Create Contact
-/**
- * opening create-dialog-window
- *
- * @function
- */
-function openCreateContact() {
-  createDialogElements.createDialog.classList.remove("resp-none");
-  createDialogElements.createDialog.classList.add("show-edit-dialog");
-  dialogBackground.classList.remove("d-none");
-}
-
-/**
- * cancelling creation of new contact
- *
- * @function
- */
-function cancelCreateContact() {
-  createDialogElements.createDialog.classList.add('resp-none');
-  createDialogElements.createDialog.classList.remove("show-edit-dialog");
-  dialogBackground.classList.add("d-none");
-}
-
-/**
- * adding new conact to contactList and to array in remote storage
- * closing create-dialog-window
- * rendering contactList with new contact
- *
- * @function
- */
-function addNewContact() {
-  let inputName = createDialogElements.inputName.value.trim();
-  inputName = capitalizeFirstLetterOfEveryWord(inputName);
-  contacts.push({
-    name: inputName,
-    email: createDialogElements.inputEmail.value.trim(),
-    number: createDialogElements.inputPhone.value.trim(),
-  });
-  document.getElementById("createContactForm").reset();
-  dialogBackground.classList.add("d-none");
-  cancelCreateContact();
-  renderContactList();
-}
-
-
-/**
- * This function is capitalizing the names of input values to generate
- * proper initial for contact list
- *
- * @param {string} input
- * @return {string}
- * 
- * @function 
- */
-function capitalizeFirstLetterOfEveryWord(input) {
-  return input.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-}
-
-/**
- * closing edit-dialog-window and create-dialog-window
- *
- * @function
- */
-function closeDialog() {
-  if (editDialogElements.editDialog.classList.contains("show-edit-dialog")) {
+  /**
+   * saving edited contact
+   *
+   * @function
+   */
+  function saveEditDialog() {
+    let index = getContactIndex(dialogElements.name.innerText);
+    /* changing values in contacts array at the index with the values from input field */
+    contacts[index].name = editDialogElements.inputName.value;
+    contacts[index].email = editDialogElements.inputEmail.value;
+    contacts[index].number = editDialogElements.inputPhone.value;
+    renderContactList();
+    changeDialogInfo(contacts[index]);
     closeEditDialog();
-  } else if (
-    createDialogElements.createDialog.classList.contains("show-edit-dialog")
-  ) {
-    cancelCreateContact();
   }
-}
 
-/**
- * show and close options-menu in responsive design
- * 
- * @function
- */
-function showResponsiveOptions() {
-  document.getElementById('responsiveOptionsContacts').classList.remove('d-none');
-}
+  // Create Contact
+  /**
+   * opening create-dialog-window
+   *
+   * @function
+   */
+  function openCreateContact() {
+    createDialogElements.createDialog.classList.remove("resp-none");
+    createDialogElements.createDialog.classList.add("show-edit-dialog");
+    dialogBackground.classList.remove("d-none");
+  }
 
-function closeResponsiveOptions() {
-  document.getElementById('responsiveOptionsContacts').classList.add('d-none')
-}
+  /**
+   * cancelling creation of new contact
+   *
+   * @function
+   */
+  function cancelCreateContact() {
+    createDialogElements.createDialog.classList.add('resp-none');
+    createDialogElements.createDialog.classList.remove("show-edit-dialog");
+    dialogBackground.classList.add("d-none");
+  }
 
-function closeWindow() {
-  document.getElementById('contactsMain').classList.add('contacts-none');
-  document.getElementById('contactsMain').classList.remove('contacts-main');
-  document.getElementById('buttonRespBack').classList.add('d-none');
-  editDialogElements.editDialog.classList.add('resp-none');
-}
+  /**
+   * adding new conact to contactList and to array in remote storage
+   * closing create-dialog-window
+   * rendering contactList with new contact
+   *
+   * @function
+   */
+  function addNewContact() {
+    let inputName = createDialogElements.inputName.value.trim();
+    inputName = capitalizeFirstLetterOfEveryWord(inputName);
+    contacts.push({
+      name: inputName,
+      email: createDialogElements.inputEmail.value.trim(),
+      number: createDialogElements.inputPhone.value.trim(),
+    });
+    document.getElementById("createContactForm").reset();
+    dialogBackground.classList.add("d-none");
+    cancelCreateContact();
+    renderContactList();
+  }
+
+
+  /**
+   * This function is capitalizing the names of input values to generate
+   * proper initial for contact list
+   *
+   * @param {string} input
+   * @return {string}
+   * 
+   * @function 
+   */
+  function capitalizeFirstLetterOfEveryWord(input) {
+    return input.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
+
+  /**
+   * closing edit-dialog-window and create-dialog-window
+   *
+   * @function
+   */
+  function closeDialog() {
+    if (editDialogElements.editDialog.classList.contains("show-edit-dialog")) {
+      closeEditDialog();
+    } else if (
+      createDialogElements.createDialog.classList.contains("show-edit-dialog")
+    ) {
+      cancelCreateContact();
+    }
+  }
+
+  /**
+   * show and close options-menu in responsive design
+   * 
+   * @function
+   */
+  function showResponsiveOptions() {
+    document.getElementById('responsiveOptionsContacts').classList.remove('d-none');
+  }
+
+  function closeResponsiveOptions() {
+    document.getElementById('responsiveOptionsContacts').classList.add('d-none')
+  }
+
+  function closeWindow() {
+    document.getElementById('contactsMain').classList.add('contacts-none');
+    document.getElementById('contactsMain').classList.remove('contacts-main');
+    document.getElementById('buttonRespBack').classList.add('d-none');
+    editDialogElements.editDialog.classList.add('resp-none');
+  }
