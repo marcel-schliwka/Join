1b4e67e311a704544b3deda65aa8e4239d9e698b
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-08-15 14:00:10 +0200

    get initials always to uppercase

--- scripts/storage.js
+++ scripts/storage.js
@@ -61,9 +61,9 @@ function isUserLoggedIn() {
 function getInitials(name) {
   const nameParts = name.trim().split(" ");
   if (nameParts.length === 1) {
-    return nameParts[0][0];
+    return nameParts[0][0].toUpperCase();
   } else {
-    return nameParts[0][0] + nameParts[1][0];
+    return nameParts[0][0].toUpperCase() + nameParts[1][0].toUpperCase();
   }
 }
 
